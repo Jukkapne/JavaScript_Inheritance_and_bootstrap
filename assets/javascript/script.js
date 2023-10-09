8f39103fc82d31c06fb26ff8c7b8b5f4d75c71e1
Jukkapne <jukka.nevalainen@oamk.fi>
2023-10-09 12:31:36 +0300

    imagelist

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 async function getProgramList() {
     const apiKey = '2ef300b1-2794-424c-880d-e136f9d73280';
-    const url = 'https://api.jwstapi.com/program/list';
+    const url = 'https://api.jwstapi.com/all/type/jpg?page=1&perPage=10';
 
     const response = await fetch(url, {
         headers: {
@@ -15,20 +15,37 @@ async function getProgramList() {
     const data = await response.json();
     return data;
 }
-console.log(getProgramList().then(data => console.log(data)));
-//create innerhtml for program list
-function createProgramList(data) {
-    const programList = document.querySelector('#program-list');
-    const programListHtml = data.map(program => {
-        return `
-        <li class="program-list-item">
-            <a href="program.html?id=${program.id}">
-                <img src="${program.image}" alt="${program.title}" class="program-list-item__image">
-                <h2 class="program-list-item__title">${program.title}</h2>
-            </a>
-        </li>
-        `;
-    }).join('');
-    programList.innerHTML = programListHtml;
+function generateList(data) {
+    console.log(data.body);
+    if (!data || !Array.isArray(data.body)) {
+        console.error('Expected data.body to be an array');
+        return;
+    }
+
+    let listHTML='';
+    for (let item of data.body) {
+        listHTML += '<img src="${item.location}" alt="Loading..." />';
+        
+        
+    }
+    
+    console.log("DYNAAMINEN HTML KOODI: "+listHTML);
+    return listHTML;
 }
-createProgramList(getProgramList().then(data => console.log(data)));
+
+
+
+async function displayData() {  
+    try {
+        const data = await getProgramList();
+        const list = generateList(data);
+        document.getElementById('programList').innerHTML = list;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+
